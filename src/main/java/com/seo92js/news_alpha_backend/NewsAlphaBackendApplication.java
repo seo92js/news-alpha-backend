82ce8303d5e236350073ccd67e0dc8f601efc655
whitescan <jeongseop@whitescan.com>
2026-03-13 09:22:05 +0900

    feat : 프로젝트 초기 세팅

---
+++ src/main/java/com/seo92js/news_alpha_backend/NewsAlphaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.seo92js.news_alpha_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAlphaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAlphaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seo92js/news_alpha_backend/NewsAlphaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seo92js.news_alpha_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAlphaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
