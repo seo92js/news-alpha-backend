d9a8af1c1e4ffaf8259f57a256990bbb7cca405d
whitescan <jeongseop@whitescan.com>
2026-03-18 15:01:47 +0900

    refactor : 패키지 경로 수정

--- src/test/java/com/seo92js/news_alpha_backend/service/AiServiceTest.java
+++ src/test/java/com/seo92js/news_alpha_backend/service/AiServiceTest.java
@@ -1,5 +1,6 @@
 package com.seo92js.news_alpha_backend.service;
 
+import com.seo92js.news_alpha_backend.ai.service.AiService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
